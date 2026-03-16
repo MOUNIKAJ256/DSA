433cba5441c823157c776372a90a0b66e68e5d51
MOUNIKAJ256 <jmouni25@gmail.com>
2026-03-16 20:35:21 +0530

    Implemented optimal approach to count maximum consecutive one's in an array

---
+++ Arrays/14_CountMaximumConsecutiveOnesInTheArray.java
@@ -0,0 +1,48 @@
+// Problem : Count Maximum Consecutive One's in the array
+// Platform : Strivers A2Z 
+// Approach : Optimal
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+import java.util.*;
+class Solution {
+    // Function to find maximum consecutive 1's in an array
+    public int findMaxConsecutiveOnes(int[] nums) {
+        // Variable to store current count of consecutive 1's
+        int cnt = 0;
+        // Variable to store maximum consecutive 1's
+        int maxi = 0;
+        // Traverse the array
+        for (int i = 0; i < nums.length; i++) {
+            // If current element is 1, increment count
+            if (nums[i] == 1) {
+                cnt++;
+            } else {
+                // If element is 0, reset count
+                cnt = 0;
+            }
+
+            // Update maximum if current count is greater
+            maxi = Math.max(maxi, cnt);
+        }
+
+        // Return maximum consecutive 1's
+        return maxi;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Input array
+        int[] nums = {1, 1, 0, 1, 1, 1};
+
+        // Create Solution object
+        Solution obj = new Solution();
+
+        // Get answer
+        int ans = obj.findMaxConsecutiveOnes(nums);
+
+        // Print result
+        System.out.println("The maximum consecutive 1's are " + ans);
+    }
+}
